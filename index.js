913f5302cadec89c898668a9bc89b73a13e343c0
jinnatul <morolswediu@gmail.com>
2020-07-14 10:03:04 +0600

    refactor all in one obj

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// export * from "./lib/basic";
-// export * from "./lib/conversion";
-// export * from "./lib/isMethod";
-// export * from "./lib/number";
-// export * from "./lib/search";
-// export * from "./lib/sort";
-
 const basic = require("./lib/basic");
 const conversion = require("./lib/conversion");
 const ismethod = require("./lib/isMethod");
@@ -12,11 +5,13 @@ const number = require("./lib/number");
 const search = require("./lib/search");
 const sort = require("./lib/sort");
 
-module.exports = {
-  basic,
-  conversion,
-  ismethod,
-  number,
-  search,
-  sort,
-};
+const mathematica = {
+  ...basic, 
+  ...conversion, 
+  ...ismethod, 
+  ...number, 
+  ...search, 
+  ...sort
+}
+
+module.exports = mathematica;
